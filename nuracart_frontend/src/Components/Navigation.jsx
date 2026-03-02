1207bd9be690fe4ef4de64cd26e6c049af773441
Arunkumar Krishnan <arnukrishnan@gmail.com>
2026-03-02 19:42:19 +0530

    fixed the vertical alignment

--- nuracart_frontend/src/Components/Navigation.jsx
+++ nuracart_frontend/src/Components/Navigation.jsx
@@ -5,7 +5,7 @@ const Navigation = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <nav className='flex px-4 py-4 justify-between items-center  md:px-18 md:py-4'>
+        <nav className='flex px-4 py-4   justify-between items-center  md:px-18 md:py-4'>
             <div>
                 <a href="/" className='text-3xl font-semibold tracking-wider'>nuracart</a>
             </div>
@@ -25,21 +25,22 @@ const Navigation = () => {
             </div>
             {/* Desktop Icons */}
             <div>
-                <ul className=' flex md:gap-10 gap-5 '>
+                <ul className=' flex items-center flex-wrap md:gap-10 gap-5 '>
                     <li><Heart className=' text-gray-600 hover:fill-black size-5' /></li>
                     <li><User className=' text-gray-600 hidden md:block hover:fill-black size-5' /></li>
                     <li><ShoppingCart className=' text-gray-600 hover:fill-black size-5' /></li>
+                    <li className='md:hidden flex '><button onClick={() => { setMenuOpen(!menuOpen) }}>{menuOpen ? <X className='size-5 text-gray-600 ' /> : <Menu className='size-5 text-gray-600 ' />}
+                    </button></li>
                 </ul>
             </div>
 
-            <div className='md:hidden'><button onClick={() => { setMenuOpen(!menuOpen) }}>{menuOpen ? <X className='size-5 text-gray-600 ' /> : <Menu className='size-5 text-gray-600 ' />}
-            </button></div>
+
 
             {menuOpen && <div className='md:hidden fixed top-0 right-0 h-full w-64 bg-white shadow-lg p-6 transition-all duration-300'>
 
 
                 <button onClick={() => setMenuOpen(false)}>
-                    <X className="size-7 font-bold text-9xl mb-6" />
+                    <X className="size-7 font-bold  mb-6" />
                 </button>
                 <ul className='flex flex-col gap-6 text-lg'>
                     <li className='py-2 text-gray-600 text-lg font-medium'>Login/Signup</li>
